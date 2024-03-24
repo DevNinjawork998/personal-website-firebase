6936bef98c3b278ee1b8b650d8a68dcb2d0fe22c
jack.ooi <jack.ooi@bp.com>
2024-03-24 13:05:07 +0800

    Made Card type

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,29 @@
 import { Heading, Image, Text } from "@chakra-ui/react";
 import ReactGA from "react-ga4";
 
-function handleClick() {
-  // Track a custom event
-  ReactGA.event({
-    category: "Button Click",
-    action: "Pokemon DataBase",
-    label: "Project Page",
-  });
-  ReactGA.event({
-    category: "Button Click",
-    action: "Simple Calculator",
-    label: "Project Page",
-  });
-}
+type CardContent = {
+  title: string;
+  description: string;
+  imageSrc: string;
+  url: string;
+};
+
+// function handleClick() {
+//   // Track a custom event
+//   ReactGA.event({
+//     category: "Button Click",
+//     action: "Pokemon DataBase",
+//     label: "Project Page Pokemon Database",
+//   });
+//   ReactGA.event({
+//     category: "Button Click",
+//     action: "Simple Calculator",
+//     label: "Project Page Simple Calculator",
+//   });
+// }
 
 // OnClick function for Car
-const Card = ({ title, description, imageSrc, url }: any) => {
+const Card = (card: CardContent) => {
   return (
     <div
       style={{
@@ -27,17 +34,17 @@ const Card = ({ title, description, imageSrc, url }: any) => {
         width: "500px",
       }}
     >
-      <a href={url} onClick={handleClick}>
-        <Image src={imageSrc} />
+      <a href={card.url} /*onClick = { handleClick }*/>
+        <Image src={card.imageSrc} />
       </a>
-      <Heading>{title}</Heading>
+      <Heading>{card.title}</Heading>
       <Text
         style={{
           fontSize: "1.2rem",
           textAlign: "center",
         }}
       >
-        {description}
+        {card.description}
       </Text>
     </div>
   );
